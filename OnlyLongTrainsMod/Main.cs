b019cbc580cc6f2e1b119e9c7e68043dc7156d03
Svetlozar Valchev <cbethax@gmail.com>
2019-03-08 04:34:55 +0200

    initial

---
+++ OnlyLongTrainsMod/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityModManagerNet;
+using Harmony12;
+using UnityEngine;
+
+namespace OnlyLongTrainsMod
+{
+    public class Main
+    {
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            // Something
+            return true; // If false the mod will show an error.
+        }
+    }
+
+    [HarmonyPatch(typeof(StationsJobLicenseUpdater), "SetStationRulesetLimits")]
+    class StationsJobLicenseUpdater_SetStationRulesetLimits_Patch
+    {
+        static void Postfix(StationProceduralJobsRuleset[] ___stationsRuleset)
+        {
+            for (int index = 0; index < ___stationsRuleset.Length; ++index)
+            {
+                ___stationsRuleset[index].minNumberOfCarsPerJob = 6;
+                ___stationsRuleset[index].maxNumberOfCarsPerJob = 10;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# dv-only-long-trains-mod
